faaf50a7e7c8ed5d53ade1a65e4f7f7588cfe49c
D-YF <daiyf1028@gmail.com>
2023-11-14 21:52:44 -0600

    document domain class

---
+++ timesheet-server/src/main/java/com/example/timesheetserver/Domain/Document.java
@@ -0,0 +1,13 @@
+package com.example.timesheetserver.Domain;
+
+public class Document {
+    private String type;
+
+    private String url;
+
+    private String title;
+
+    private String uploadedBy;
+
+    private String uploadedTime;
+}
