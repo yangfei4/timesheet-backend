54bb19f525c593e0cc50891604f668d26d4d298d
D-YF <daiyf1028@gmail.com>
2023-11-14 21:52:06 -0600

    profile-relevant domain class

---
+++ timesheet-server/src/main/java/com/example/timesheetserver/Domain/DailyTimesheet.java
@@ -0,0 +1,17 @@
+package com.example.timesheetserver.Domain;
+
+public class DailyTimesheet {
+    private String day;
+
+    private String date;
+
+    private String startingTime;
+
+    private String endingTime;
+
+    private boolean isFloatingDay;
+
+    private boolean isVacationDay;
+
+    private boolean isHoliday;
+}
