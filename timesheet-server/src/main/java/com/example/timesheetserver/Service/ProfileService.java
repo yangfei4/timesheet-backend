dcf70d0e3637e03d31e32e161b0a99782290285c
D-YF <daiyf1028@gmail.com>
2023-11-15 15:16:40 -0600

    profile service with updateWeeklyTimesheetTemplate and getAllProfiles

---
+++ timesheet-server/src/main/java/com/example/timesheetserver/Service/ProfileService.java
@@ -0,0 +1,34 @@
+package com.example.timesheetserver.Service;
+
+import com.example.timesheetserver.DAO.ProfileRepository;
+import com.example.timesheetserver.Domain.DailyTimesheet;
+import com.example.timesheetserver.Domain.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProfileService {
+    ProfileRepository profileRepository;
+
+    @Autowired
+    ProfileService (ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public List<DailyTimesheet> updateWeeklyTimesheetTemplate(String id, List<DailyTimesheet> template) {
+        Optional<Profile> optional = profileRepository.findById(id);
+        optional.ifPresent(profile -> {
+            profile.setWeeklyTimesheetTemplate(template);
+            profileRepository.save(profile);
+        });
+        return template;
+    }
+
+    public List<Profile> getAllProfiles() {
+        return profileRepository.findAll();
+    }
+
+}
