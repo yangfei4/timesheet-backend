9195a7b713a2c741e8ff6a2514c2486173852d39
D-YF <daiyf1028@gmail.com>
2023-11-22 21:45:56 -0600

    eureka-server module with configuration

--- eureka-server/src/main/java/com/example/eurekaserver/ServletInitializer.java
+++ eureka-server/src/main/java/com/example/eurekaserver/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.timesheetbackend;
+package com.example.eurekaserver;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TimesheetBackendApplication.class);
+        return application.sources(EurekaServerApplication.class);
     }
 
 }
--- eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.timesheetbackend;
+package com.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TimesheetBackendApplicationTests {
+class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
