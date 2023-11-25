857c4aed91dd332edde00e738a8ff0c257c58a16
D-YF <daiyf1028@gmail.com>
2023-11-25 12:50:08 -0600

    scheduled generateTimesheetEverySunday service

--- timesheet-server/src/main/java/com/example/timesheetserver/Service/TimesheetService.java
+++ timesheet-server/src/main/java/com/example/timesheetserver/Service/TimesheetService.java
@@ -1,16 +1,19 @@
 package com.example.timesheetserver.Service;
 
-import com.example.timesheetserver.Domain.Profile;
-import com.example.timesheetserver.Domain.Timesheet;
-import com.example.timesheetserver.Domain.WeeklyTimesheet;
+import com.example.timesheetserver.Domain.*;
+import com.example.timesheetserver.Util.TimeManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.aggregation.DateOperators;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import com.example.timesheetserver.DAO.TimesheetRepository;
 import com.example.timesheetserver.DAO.ProfileRepository;
 
 import javax.swing.text.html.Option;
+import java.nio.file.WatchKey;
 import java.sql.Time;
+import java.text.ParseException;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
 import java.util.PrimitiveIterator;
@@ -49,6 +52,37 @@ public class TimesheetService {
     // TODO(Yangfei): implement this method
     public void updateTimesheet(String profileId, WeeklyTimesheet weeklyTimesheet) {
         String weekEnding = weeklyTimesheet.getWeekEnding();
+    }
+
+    // Will be called at midnight (0 hours, 0 minutes, 0 seconds) every Sunday
+    @Scheduled(cron = "0 0 0 ? * SUN *")
+    public void generateTimesheetEverySunday() throws DateTimeParseException {
+        List<Profile> profileList = profileRepository.findAll();
+        for(Profile profile: profileList) {
+            String weekEnding = TimeManager.getWeekEndingOfCurrentWeek(); // e.g. 2023-11-25
+            List<DailyTimesheet> template = profile.getWeeklyTimesheetTemplate();
+            Timesheet timesheet = new Timesheet();
+            timesheet.setProfile(profile);
+            WeeklyTimesheet weeklyTimesheet = new WeeklyTimesheet();
+            weeklyTimesheet.setWeekEnding(weekEnding);
+            weeklyTimesheet.setSubmissionStatus("Not Started");
+            weeklyTimesheet.setApprovalStatus("N/A");
+            weeklyTimesheet.setTotalBillingHours(45);
+            weeklyTimesheet.setTotalCompensatedHours(45);
+            weeklyTimesheet.setFloatingDayUsed(0);
+            weeklyTimesheet.setHolidayUsed(0);
+            weeklyTimesheet.setVacationDayUsed(0);
+            weeklyTimesheet.setDailyTimesheets(TimeManager.setAllDatesByWeekEnding(template, weekEnding));
+            Document document = new Document();
+            document.setType("");
+            document.setUrl("");
+            document.setTitle("");
+            document.setUploadedBy(profile.getName());
+            document.setUploadedTime("");
+            weeklyTimesheet.setDocument(document);
+            timesheet.setWeeklyTimesheet(weeklyTimesheet);
+
+        };
     }
 
     /*
--- timesheet-server/src/main/java/com/example/timesheetserver/Util/TimeManager.java
+++ timesheet-server/src/main/java/com/example/timesheetserver/Util/TimeManager.java
@@ -1,9 +1,19 @@
 package com.example.timesheetserver.Util;
 
+import com.example.timesheetserver.Domain.DailyTimesheet;
+import de.jollyday.Holiday;
+import de.jollyday.HolidayCalendar;
+import de.jollyday.HolidayManager;
+
+import java.text.ParseException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class TimeManager {
     public static String getCurrentTime() {
@@ -13,10 +23,47 @@ public class TimeManager {
     }
 
     public static String getWeekEndingOfCurrentWeek() {
-        LocalDate currentDate =  LocalDate.now();
+        LocalDate currentDate = LocalDate.now();
         DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
         int dayUntilSaturday = DayOfWeek.SATURDAY.getValue() - currentDayOfWeek.getValue();
         LocalDate weekEndingOfCurrentWeek = currentDate.plusDays(dayUntilSaturday);
         return weekEndingOfCurrentWeek.toString(); //e.g. 2023-11-25
     }
+
+    public static List<DailyTimesheet> setAllDatesByWeekEnding(List<DailyTimesheet> template, String weekEnding) throws DateTimeParseException {
+        for(DailyTimesheet dailyTimesheet:template) {
+            String date = "";
+            List<String> weekdays = Arrays.asList("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+            int index = weekdays.indexOf(dailyTimesheet.getDay());
+            if(index != -1) {
+                int move = 6 - index;
+                date = generateDateByCurrentWeekday(weekEnding, move);
+                dailyTimesheet.setDate(date);
+                if(isHoliday(date)) {
+                    dailyTimesheet.setHoliday(true);
+                }
+            } else {
+                System.out.println("Wrong weekday format");
+            }
+        }
+        return template;
+    }
+
+    public static boolean isHoliday(String date) {
+        HolidayManager holidayManager = HolidayManager.getInstance(HolidayCalendar.UNITED_STATES);
+        Set<Holiday> holidays = holidayManager.getHolidays(2023, "ny");
+        for(Holiday holiday: holidays) {
+            if(date.equals(String.valueOf(holiday.getDate()))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String generateDateByCurrentWeekday(String weekEnding, int move) throws DateTimeParseException {
+        LocalDate weekEndingDate = LocalDate.parse(weekEnding);
+        LocalDate resultDate = weekEndingDate.minusDays(move);
+        return resultDate.toString();
+    }
+
 }
