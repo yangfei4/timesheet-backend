2f0e12b70286278109ba06ccb6cd6f79bb9d7a98
D-YF <daiyf1028@gmail.com>
2023-11-24 22:49:55 -0600

    Refactor TimeManager using java.time library

--- timesheet-server/src/main/java/com/example/timesheetserver/Util/TimeManager.java
+++ timesheet-server/src/main/java/com/example/timesheetserver/Util/TimeManager.java
@@ -1,14 +1,22 @@
 package com.example.timesheetserver.Util;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TimeManager {
     public static String getCurrentTime() {
-        SimpleDateFormat sdf = new SimpleDateFormat();
-        sdf.applyPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        Date data = new Date();
-        return sdf.format(data);
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        return currentTime.format(formatter); //2023-11-22T15:45:30Z
+    }
+
+    public static String getWeekEndingOfCurrentWeek() {
+        LocalDate currentDate =  LocalDate.now();
+        DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
+        int dayUntilSaturday = DayOfWeek.SATURDAY.getValue() - currentDayOfWeek.getValue();
+        LocalDate weekEndingOfCurrentWeek = currentDate.plusDays(dayUntilSaturday);
+        return weekEndingOfCurrentWeek.toString(); //e.g. 2023-11-25
     }
 }
