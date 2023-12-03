cbcf372ca4a03d36635c40a0ea5e347a84c6ab17
D-YF <daiyf1028@gmail.com>
2023-12-03 16:46:36 -0600

    adjusted home icon

--- timesheet-frontend/src/components/Navbar/Navbar.js
+++ timesheet-frontend/src/components/Navbar/Navbar.js
@@ -4,8 +4,10 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <NavLink className="navbar-brand" to="/">Timesheet</NavLink>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light container-fluid">
+            <NavLink className="navbar-brand m-0" to="/">
+                <img src='https://www.freeiconspng.com/uploads/website-icon-21.png' width="50" type="image" padding='10px' />
+            </NavLink>
             <div className="navbar-nav">
                 <NavLink className="nav-item nav-link" to="/summary">Summary</NavLink>
                 <NavLink className="nav-item nav-link" to="/timesheet">Timesheet</NavLink>
