28b4cfedbbae605449a6009fdb1983b857051ff2
D-YF <daiyf1028@gmail.com>
2023-12-03 16:45:23 -0600

    App component

--- timesheet-frontend/src/App.js
+++ timesheet-frontend/src/App.js
@@ -1,17 +1,29 @@
 import './App.scss';
 
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Summary from './components/Summary';
 import Timesheet from './components/Timesheet';
 import Profile from './components/Profile';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import WelcomePage from './components/Pages/WelcomePage';
+import NotFoundPage from './components/Pages/NotFoundPage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function isLoggedIn() {
+  let curJWT = localStorage.getItem('JWT');
+  let curUserId = localStorage.getItem('userId');
+  // return curJWT && curUserId;
+  return true;
+}
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <div className="App">
+    isLoggedIn() ? 
+      (<div className="App">
         <header className="App-header">
           <Navbar />
         </header>
@@ -19,9 +31,15 @@ function App() {
           <Route path="/summary" element={<Summary/>} />
           <Route path="/timesheet" element={<Timesheet/>} />
           <Route path="/profile" element={<Profile/>} />
+          <Route path="/" element={<Navigate to="/summary" />} />
+          <Route path="*" element={<NotFoundPage/>} />
         </Routes>
       </div>
-    </BrowserRouter>
+      )
+      :
+      (
+        <WelcomePage />
+      )
   );
 }
 
--- timesheet-frontend/src/index.js
+++ timesheet-frontend/src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import appReducer from './reducers/reducer-store';
+
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({ reducer: appReducer });
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
