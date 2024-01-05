c39d27f2af66243387cfcdf79e04e6de710d587a
D-YF <daiyf1028@gmail.com>
2024-01-04 22:20:33 -0600

    UserService implements UserDetailsService with loadUserByUsername method

---
+++ auth-server/src/main/java/com/example/authserver/Service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.authserver.Service;
+
+import com.example.authserver.DAO.UserRepository;
+import com.example.authserver.Domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()
+                -> new UsernameNotFoundException("Username " + username + " not found"));
+    }
+}
