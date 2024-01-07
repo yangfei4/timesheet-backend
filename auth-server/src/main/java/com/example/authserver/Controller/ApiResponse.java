6294332ce931c17095a777659ce7fd2c66c699d0
D-YF <daiyf1028@gmail.com>
2024-01-07 16:31:35 -0600

    added Api Response Class to Auth-server

---
+++ auth-server/src/main/java/com/example/authserver/Controller/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.example.authserver.Controller;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private String message;
+    private T data;
+}
