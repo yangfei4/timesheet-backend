d3a95f077cde32bbb12e5e7e73ac07b2c6df2068
D-YF <daiyf1028@gmail.com>
2024-01-07 16:31:11 -0600

    minor change(adding ToString annotation

--- auth-server/src/main/java/com/example/authserver/Requests/LoginRequest.java
+++ auth-server/src/main/java/com/example/authserver/Requests/LoginRequest.java
@@ -1,14 +1,12 @@
 package com.example.authserver.Requests;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class LoginRequest {
     private String username;
     private String password;
