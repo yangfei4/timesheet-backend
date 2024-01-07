65be178f442276e2ac64e7aa58805f6e94af2d31
D-YF <daiyf1028@gmail.com>
2024-01-07 16:45:49 -0600

    profile Feign client

---
+++ auth-server/src/main/java/com/example/authserver/Feign/ProfileClient.java
@@ -0,0 +1,14 @@
+package com.example.authserver.Feign;
+
+import com.example.authserver.Controller.ApiResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "profile-server")
+public interface ProfileClient {
+
+    @PostMapping("/profile/create")
+    public ResponseEntity<String> createProfileFromAuth(@RequestBody String username);
+}
