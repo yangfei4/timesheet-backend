c2dd4179789327d18f6e4f2d419eb27d26f7c160
D-YF <daiyf1028@gmail.com>
2024-01-21 21:55:20 -0600

    added redis dependency and make profile server cacheable

--- profile-server/src/main/java/com/example/profileserver/ProfileServerApplication.java
+++ profile-server/src/main/java/com/example/profileserver/ProfileServerApplication.java
@@ -2,10 +2,12 @@ package com.example.profileserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCaching
 public class ProfileServerApplication {
 
 	public static void main(String[] args) {
